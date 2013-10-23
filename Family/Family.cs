a61f12c82c2c7b2cf24cff64d23bef5b2e3dfe8e
EmilieS <simonard@intechinfo.fr>
2013-10-23 14:15:19 +0200

    Classes Family and Villager.

---
+++ Family/Family.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Family
+{
+    class Family
+    {
+        //TODO initial constructor.
+        public Family(Villager mother, Villager father)
+        {
+            _goldStash = mother.ParentFamily.takeFromGoldStash(mother.ParentFamily.GoldStash / 10); //10%
+            _goldStash += father.ParentFamily.takeFromGoldStash(father.ParentFamily.GoldStash / 10); //10%
+            _mother = mother;
+            _father = father;
+            _mother.StatusInFamily = Status.MARRIED;
+            _father.StatusInFamily = Status.MARRIED;
+            _familyMembers=new List<Villager>();
+            _familyMembers.Add(_mother);
+            _familyMembers.Add(_father);
+            _mother.ParentFamily = this;
+            _father.ParentFamily = this;
+        }
+        int _goldStash;
+        Villager _mother;
+        Villager _father;
+        readonly List<Villager> _familyMembers;
+
+        public int GoldStash { get { return _goldStash; } }
+        public Villager Mother { get { return _mother; } }
+        public Villager Father { get { return _father; } }
+
+        public void newFamilyMember()
+        {
+            _familyMembers.Add(new Villager(this));
+        }
+        /// <summary>
+        /// takes from the gold stash the amount asked, if not the maximum it can. Returns true amount.
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns></returns>
+        public int takeFromGoldStash(int amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (amount<=_goldStash)
+            {
+                _goldStash -= amount;
+                return amount;
+            }
+            int goldLeft=_goldStash;
+            _goldStash=0;
+            return goldLeft;
+        }
+
+
+    }
+}
---
+++ Family/VillagerProperties.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Family
+{
+    //public class VillagerProperties
+    //{
+        enum  Genders
+        {
+            MALE = false,
+            FEMALE = true
+        };
+        enum Jobs
+        {
+            FARMER = 0,
+            CONSTRUCTION_WORKER = 1,
+            COOK = 10,
+
+        };
+        enum Healths
+        {
+            DEAD = 0,
+            HEATHY = 1,
+            SICK = 2
+        };
+
+        enum Status
+        {
+            SINGLE = 0,
+            MARRIED = 1,
+            ENGAGED = 2
+        }
+    //}
+}
