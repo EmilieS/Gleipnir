56757f8c9617cb3e63966aff5a4e0e3a90e6297b
myzaki <mougel@intechinfo.fr>
2013-10-27 11:19:55 +0100

    Crafters and Materials

---
+++ Jobs/Crafter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jobs
+{
+    class Crafter
+    {
+        int _creationLifeLevel;
+
+        public Crafter()
+        {
+            materials = new Crafter.Materials.CrafterMaterials();
+        }
+        
+        public int CreationLifeLevel
+        {
+            get{return _creationLifeLevel;}
+        }
+        public double Effectiveness
+        {
+            
+        }
+    }
+}
---
+++ Updates/Crafter/Materials/CrafterMaterials.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Updates.Crafter.Materials
+{
+    public enum CrafterMaterials
+    {
+        WhiteWash = 200,
+        Cement = 1000,
+        Reinforcement = 10000
+    }
+}
