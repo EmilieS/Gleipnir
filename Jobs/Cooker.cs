a000f419b76a0aa4d528b981161b12bb727840b3
LS4Tonio <toniolesniper@gmail.com>
2013-10-28 09:36:58 +0100

    Create Jobs objects

---
+++ Jobs/Cooker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jobs
+{
+    class Cooker
+    {
+        List<object> _Persons;
+
+        public Cooker()
+        {
+            _Persons.Add(Villager.Name);
+        }
+
+        public void AddPerson()
+        {
+        }
+
+        public void RemovePerson()
+        {
+        }
+
+        public void GenerateGold()
+        {
+        }
+
+        public void AddHappiness()
+        {
+        }
+    }
+}
---
+++ Jobs/Farmer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jobs
+{
+    class Farmer
+    {
+        List<object> _Persons;
+
+        public Farmer()
+        {
+            _Persons.Add(Villager.Name);
+        }
+
+        public void AddPerson()
+        {
+        }
+
+        public void RemovePerson()
+        {
+        }
+
+        public void GenerateGoldAndHappiness()
+        {
+        }
+
+        public void ModifyGoldGeneration()
+        {
+        }
+    }
+}
