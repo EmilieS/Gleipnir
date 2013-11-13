3a9c7729d62e8c5679e370d7f19c0fc15975f76f
LS4Tonio <toniolesniper@gmail.com>
2013-11-13 10:16:02 +0100

    Jobs heritage - Modified gold & happiness generation

--- GameJobs/Miller.cs
+++ GameJobs/Miller.cs
@@ -8,99 +8,34 @@ using System.Threading.Tasks;
 
 namespace GameJobs
 {
-    public class Miller : INotifyPropertyChanged
+    public class Miller : JobsModel //: INotifyPropertyChanged
     {
-        double _gold;
-        List<Villager> _workers;
-        Jobs _job;
-
         public Miller()
         {
-            _gold = 65;
             _job = Jobs.MILLER;
+            jobName = "Meunier";
             _workers = new List<Villager>();
-        }
-
-        /// <summary>
-        /// Gets Gold amount is generate per tick
-        /// </summary>
-        public double Gold
-        {
-            get { return _gold; }
-        }
-
-        /// <summary>
-        /// Gets the Workers list
-        /// </summary>
-        public List<Villager> Workers
-        {
-            get { return _workers; }
-        }
-
-        /// <summary>
-        /// Add a Villager to the Job
-        /// </summary>
-        /// <param name="person"></param>
-        public void AddPerson(Villager person)
-        {
-            if (person.Job == 0)
-            {
-                person.setJob(_job);
-                _workers.Add(person);
-            }
-        }
-
-        /// <summary>
-        /// Remove the Villager from the Job
-        /// </summary>
-        /// <param name="person"></param>
-        public void RemovePerson(Villager person)
-        {
-            if (person.Job > 0)
-            {
-                person.setJob(Jobs.NONE);
-                _workers.Remove(person);
-            }
-        }
-
-        /// <summary>
-        /// Add gold to workers
-        /// </summary>
-        public void GenerateGold()
-        {
+            coefficient = 13;
             _gold = ModifyGoldGeneration();
-            foreach (Villager person in _workers)
-            {
-                person.AddGoldInWallet(_gold);
-            }
-        }
-
-        /// <summary>
-        /// Less Gold generation if many job workers
-        /// </summary>
-        /// <returns></returns>
-        public double ModifyGoldGeneration()
-        {
-            if (_workers.Count > 1)
-            {
-                _gold = _gold - (_workers.Count - 1);
-            }
-            else
-            {
-                _gold = 75;
-            }
-            return _gold;
         }
         
         /// <summary>
         /// Add amount of happiness for all others villagers
         /// </summary>
         /// <param name="person"></param>
-        public void AddHappiness(Villager person)
+        public override void AddHappiness()
         {
-            person.AddOrRemoveHappiness(7);
+            foreach (Villager v in Village) // DONT WORK FOR THE MOMENT
+            {
+                v.AddOrRemoveHappiness(7);
+            }
+
+            foreach (Villager person in _workers)
+            {
+                person.AddOrRemoveHappiness(-7);
+            }
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        // public event PropertyChangedEventHandler PropertyChanged;
     }
 }
