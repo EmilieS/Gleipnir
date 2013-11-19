119c72efac111cbd6c504c5081b13b0f2b0a1140
myzaki <mougel@intechinfo.fr>
2013-11-19 14:26:24 +0100

    Begin Test Meeting

---
+++ Tests/TestsMeeting.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game;
+
+namespace Tests
+{
+    [TestFixture]
+    class TestsMeeting
+    {
+        Game.Game g = new Game.Game();
+        
+        [Test]
+        public void TestChangingActivity()
+        {
+
+
+        }
+    }
+}
