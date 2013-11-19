c9caf723ce98daab967591b1edfd08bec2e553c0
myzaki <mougel@intechinfo.fr>
2013-11-19 14:56:59 +0100

    TestMeeting ended

--- Tests/TestsMeeting.cs
+++ Tests/TestsMeeting.cs
@@ -11,12 +11,23 @@ namespace Tests
     [TestFixture]
     class TestsMeeting
     {
-        Game.Game g = new Game.Game();
-        
+            
         [Test]
         public void TestChangingActivity()
         {
-
+            var MyGame = new Game.Game();
+            var village = MyGame.Villages[0];
+            Family family = village.FamiliesList[0];
+            Villager mother = family.Mother;
+            Villager father = family.Father;
+            Family family2 = MyGame.Villages[0].FamiliesList[1];
+            Villager mother2 = family2.Mother;
+            Villager father2 = family2.Father;
+            var meeting = new Meeting();
+            meeting.ChangeVillagersStatus(family);
+            
+            Assert.That((mother.ActivityStatus & father.ActivityStatus) != 0);
+            Assert.That((mother.ActivityStatus & mother2.ActivityStatus) == 0);
 
         }
     }
