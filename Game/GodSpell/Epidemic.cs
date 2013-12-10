83009525ee4be48e2f00f82dcca1d700a44036c9
myzaki <mougel@intechinfo.fr>
2013-12-10 15:00:57 +0100

    Begin of Godspells

---
+++ Game/GodSpell/Epidemic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.GodSpell
+{
+    public class Epidemic
+    {
+        internal Epidemic(Game g)
+        {
+
+        }
+        public void EveryBodysSick()
+        {
+
+        }
+    }
+}
