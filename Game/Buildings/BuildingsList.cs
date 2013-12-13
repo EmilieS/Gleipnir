8fc86072611aee293530588308c4eaf422efd403
EmilieS <simonard@intechinfo.fr>
2013-12-13 10:54:33 +0100

    a couple additions.

--- Game/Buildings/BuildingsList.cs
+++ Game/Buildings/BuildingsList.cs
@@ -38,37 +38,7 @@ namespace Game.Buildings
         public  IReadOnlyList<BuildingsModel> TheaterList{ get { return _theaterList; } }
         public  IReadOnlyList<BuildingsModel> UnionOfCrafterList { get { return _unionOfCrafterList; } }
 
-        /*
-        #region oldcode
-        readonly Baths _baths;
-        readonly Brothel _brothel;
-        readonly Chapel _chapel;
-        readonly Farm _farm;
-        readonly Forge _forge;
-        readonly Mill _mill;
-        readonly OfferingWarehouse _offeringWarehouse;
-        readonly PartyRoom _partyRoom;
-        readonly Restaurant _restaurant;
-        readonly TablePlace _tablePlace;
-        readonly Tavern _tavern;
-        readonly Theater _theater;
-        readonly UnionOfCrafter _unionOfCrafter;
-
-        public BuildingsModel Baths { get { return _baths; } }
-        public BuildingsModel Brothel { get { return _brothel; } }
-        public BuildingsModel Chapel { get { return _chapel; } }
-        public BuildingsModel Farm { get { return _farm; } }
-        public BuildingsModel Forge { get { return _forge; } }
-        public BuildingsModel Mill { get { return _mill; } }
-        public BuildingsModel OfferingWarehouse { get { return _offeringWarehouse; } }
-        public BuildingsModel PartyRoom { get { return _partyRoom; } }
-        public BuildingsModel Restaurant { get { return _restaurant; } }
-        public BuildingsModel TablePlace { get { return _tablePlace; } }
-        public BuildingsModel Tavern { get { return _tavern; } }
-        public BuildingsModel Theater { get { return Theater; } }
-        public BuildingsModel UnionOfCrafter { get { return _unionOfCrafter; } }
-        #endregion
-        */
+       
 
         readonly List<BuildingsModel> _buildingsList;
 
@@ -91,36 +61,7 @@ namespace Game.Buildings
             _tavernList = new List<Buildings.Tavern>();
             _theaterList = new List<Buildings.Theater>();
             _unionOfCrafterList = new List<Buildings.UnionOfCrafter>();
-            /*
-            #region old code
-            _baths = new Baths(village, this, "Bains");
-            _brothel = new Brothel(village, this, "Bordel");
-            _chapel = new Chapel(village, this, "Chapelle");
-            _farm = new Farm(village, this, "Ferme");
-            _forge = new Forge(village, this, "Forge");
-            _mill = new Mill(village, this, "Moulin");
-            _offeringWarehouse = new OfferingWarehouse(village, this, "Entrepôt");
-            _partyRoom = new PartyRoom(village, this, "Salle de Fête");
-            _restaurant = new Restaurant(village, this, "Restaurant");
-            _tablePlace = new TablePlace(village, this, "Table");
-            _tavern = new Tavern(village, this, "Taverne");
-            _theater = new Theater(village, this, "Théatre");
-            _unionOfCrafter = new UnionOfCrafter(village, this, "Syndicat des Ouvriers");
-            _buildingsList.Add(_baths);
-            _buildingsList.Add(_brothel);
-            _buildingsList.Add(_chapel);
-            _buildingsList.Add(_farm);
-            _buildingsList.Add(_forge);
-            _buildingsList.Add(_mill);
-            _buildingsList.Add(_offeringWarehouse);
-            _buildingsList.Add(_partyRoom);
-            _buildingsList.Add(_restaurant);
-            _buildingsList.Add(_tablePlace);
-            _buildingsList.Add(_tavern);
-            _buildingsList.Add(_theater);
-            _buildingsList.Add(_unionOfCrafter);
-            #endregion
-            */
+
         }
         internal void Add(ApothecaryOffice apothecaryOffice)
         {
